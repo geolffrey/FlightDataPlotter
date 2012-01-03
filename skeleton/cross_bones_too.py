36617baedbd5fa38823ebd8b445a806c06f6e99c
Martin Wimpress <martin.wimpress@flightdataservices.com>
2012-01-03 16:21:01 +0000

    PEP8 fixes

--- skeleton/cross_bones_too.py
+++ skeleton/cross_bones_too.py
@@ -2,12 +2,12 @@
 
 
 def cross_bones():
-	print('Cross Bones Too')
+    print('Cross Bones Too')
 
 
 def run():
-	cross_bones()
+    cross_bones()
 
-	
+
 if __name__ == '__main__':
-	run()
+    run()
