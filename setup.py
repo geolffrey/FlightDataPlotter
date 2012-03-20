f15da433987ba39264529513927501187337453b
Martin Wimpress <martin.wimpress@flightdataservices.com>
2012-03-20 17:18:31 +0000

    Migrated to new setup.py and Sphinx conf

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 # Copyright (c) Flight Data Services Ltd
 # http://www.flightdataservices.com
 # See the file "LICENSE" for the full license governing this code.
@@ -49,3 +48,6 @@ setup(
         #skull_gui = skeleton.gui.skull_gui:main
         """,                       
 )
+
+################################################################################
+# vim:et:ft=python:nowrap:sts=4:sw=4:ts=4
