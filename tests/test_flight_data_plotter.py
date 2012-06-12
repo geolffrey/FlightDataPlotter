76c0b3af0f6e2ee1c0120bfd6e1d727bf7150353
Glen Kirkup <glen.kirkup@flightdataservices.com>
2012-06-12 13:51:15 +0100

    Created bogus test for jenkins.

---
+++ tests/test_flight_data_plotter.py
@@ -0,0 +1,32 @@
+################################################################################
+
+
+'''
+'''
+
+
+################################################################################
+# Imports
+
+
+import unittest
+
+
+################################################################################
+# Test Cases
+
+
+class TestFlightDataPlotter(unittest.TestCase):
+    '''
+    '''
+    pass
+
+    def test_flight_data_plotter(self):
+        '''
+        Does Jenkins require at least one passing test to make a build?
+        '''
+        pass
+
+
+################################################################################
+# vim:et:ft=python:nowrap:sts=4:sw=4:ts=4
